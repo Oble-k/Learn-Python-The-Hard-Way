f1e6429e826bef0376b5d87cc7cef36b4e23a4b4
Germán <germanders86@gmail.com>
2026-03-12 13:14:54 +0100

    exercise 15,16,17 check at home. Ex18 not finished

--- ex18.py
+++ ex18.py
@@ -1,8 +1,8 @@
 #Names, Variables, Code, Functions
-def print_two(*args):
+def print_two(*args): #This defines a function named print_two (name could be anything)// We want *args// We end the line with a colon (:) and we start indenting lines (Tabbbing kind of)
     arg1,arg2 = args
     print(f"arg1: {arg1}, arg2: {arg2}")
-
+#
 def print_two_again(arg1, arg2):
     print(f"arg1: {arg1}, arg2: {arg2}")
 
@@ -11,7 +11,6 @@ def print_one(arg1):
 
 def print_none():
     print("I got nothing.")
-
 
 print_two("Zed", "Shaw")
 print_two_again("Zed", "Shaw")
