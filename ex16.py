cd6cf48ec0ea7d167d7be56a6964774ec98a8cfc
Germán <germanders86@gmail.com>
2026-03-15 16:09:43 +0100

    Exercises 1- 14 moved to a folder. Exercises up to the 18th done. need to revisit 17 at some point

--- ex16.py
+++ ex16.py
@@ -1,3 +1,4 @@
+# Reading and Writing files
 from sys import argv
 
 script, filename = argv #In the terminal we give the name of the file
@@ -8,19 +9,25 @@ print("If you do want that, hit RETURN.")#Another print
 input("?")#An input that really doesn't matter what you put on it, as it can't save the info
 
 print("Opening the file...")
+# target = open(filename) #Let's try and remove the mode w from  the open.
+# It seems that an error occurs in the truncate function. It can't write on the file, because by default it is in read mode only.
+#And thus it is not possible to truncate, if the file is opened in the read mode
 target = open(filename, 'w') #Open is in mode w, which stands for write
+# print("Truncating the file. Goodbye!")
+# target.truncate() #If the file is not on write mode there is an error: io.UnsupportedOperation: truncate
+#There is also one question left. If the file test.txt is empty, maybe there is no reason to truncate it. But what if it is not empty?.
 
-print("Truncating the file. Goodbye!")
-target.truncate()
-
+#I believe that, if you open a file in write mode, it truncates the file first. Thats what I found on the internet: https://docs.python.org/3/library/functions.html
+#That is why it is not required to target.truncate() the file
 print("Now I'm going to ask you for three lines.")
 
 line1 = input("line 1: ")
 line2 = input("line 2: ")
 line3 = input("line 3: ")
 
-print("I'm going to write these on the file.")
+print("I'm going to write these on the file.\n")
 lines=f"{line1}\n{line2}\n{line3}"
+# Study drill 3
 target.write(lines) #With this and the previous line we managed to write lines on the txt file using only a single target.write
 # target.write(line1, "\n", line2, "\n", line3,"\n") #This one does NOT work
 
@@ -32,7 +39,7 @@ target.write(lines) #With this and the previous line we managed to write lines o
 # target.write("\n")
 
 #Study drill 2, in the next 3 lines of code.
-print("Now we will read the file")
+print("Now we will read the file:\n")
 
 target = open(filename, 'r') #Remember, if we want to read a file, its important first to change it to mode r (Read)
 print(target.read())
--- ex18.py
+++ ex18.py
@@ -2,7 +2,9 @@
 def print_two(*args):
     arg1,arg2 = args
     print(f"arg1: {arg1}, arg2: {arg2}")
-
+# What does the asterisks in *args do?
+# It tells python to take all the arguments to the function and then put them in args as a list.
+# It is really similar to argv but for functions
 def print_two_again(arg1, arg2):
     print(f"arg1: {arg1}, arg2: {arg2}")
 
