54733b64c3817c5eb65d518e7b401f1839224ed7
Germán <germanders86@gmail.com>
2026-03-27 14:09:58 +0100

    Ex31 completed. Created a new folder for ex 15-30

---
+++ Exercises15-30/ex28.py
@@ -0,0 +1,54 @@
+# Boolean Practice (No code here, just the prompts that I wrote in the terminal
+>>> True and True
+True
+>>> False and True
+False
+>>> 1==1 and 2==1
+False
+
+>>> "test"=="test"
+True
+>>> 1==1 or 2!=1
+True
+>>> True and 1==1
+True
+>>> False and 0!=0
+False
+>>> True or 1==1
+True
+>>> "test"=="testing"
+False
+>>> 1!=0 and 2 ==1
+False
+
+>>> "test"!="testing"
+True
+
+>>> "test" ==1
+False
+>>> not(True and False)
+True
+>>> not(1==1 and 0!=1)
+False
+>>> not(True and False)
+True
+>>> not(10 == 1 or 1000==1000)
+False
+
+>>> not(1!=10 or 3==4)
+False
+>>> not("testing"=="testing" and "Zed"=="cool guy")
+True
+>>> 1==1 and (not("testing"==1 or 1==0))
+True
+>>> "chunky"=="bacon" and (not(3==4 or 3==3))
+False
+>>> 3==3 and(not("testing" == "testing" or "Python"=="Fun"))
+False
+>>> "test" and "test"
+'test'
+>>> "test" and True
+True
+>>> "test" and False
+False
+>>>
