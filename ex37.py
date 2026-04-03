c82fb4c40581a2405172b4f181406531bf4940e3
Germán <germanders86@gmail.com>
2026-04-03 16:53:55 +0200

    ex 36 in proccess, ex 37 as wellll

---
+++ ex37.py
@@ -0,0 +1,10 @@
+#Symbol review
+# I'm gonna try some keywords to learn what they do:
+print("Hey")
+# If the content inside of assert is fase, an exception will be raised, in this case the AssertionError
+assert(1==1),"aaaa estas sequit" #nothing
+
+# assert(1==2) #AssertionError
+print("Hey")
+assert False, "Aaaaa te bañastes"
+print("Hey")
