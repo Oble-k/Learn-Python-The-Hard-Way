f1fb9dac9bddecf709fb557dbd6dfb3b3d65b650
Germán <germanders86@gmail.com>
2026-05-11 22:09:43 +0200

    ex 41 completed

--- oop_test.py
+++ oop_test.py
@@ -6,6 +6,7 @@ import sys
 WORD_URL = "http://learncodethehardway.org/words.txt"
 WORDS = []
 
+
 PHRASES = {
     "class %%%(%%%):":
         "Make a class named %%% that is-a %%%.",
@@ -18,7 +19,7 @@ PHRASES = {
     "***.***(@@@)":
         "From *** get the *** function, call it with params self, @@@.",
     "***.*** = '***'":
-        "From *** get the *** atribute and set it to '***'."
+        "From *** get the *** attribute and set it to '***'."
 }
 
 # do they want to drill phrases first
@@ -26,7 +27,7 @@ if len(sys.argv) == 2 and sys.argv[1] == "english":
     PHRASE_FIRST = True
 else:
     PHRASE_FIRST = False
-
+print(PHRASE_FIRST)
 # load up the words from the website
 for word in urlopen(WORD_URL).readlines():
     WORDS.append(str(word.strip(),encoding = "utf-8"))
